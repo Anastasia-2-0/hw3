4608cebb777d33e8ffdc2878c74316825e218d48
Anastasia-2-0 <nosova0702@mail.ru>
2022-10-10 13:44:35 +0300

    решение 2 задачи

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите координаты x точки А");
+double ax = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты y точки А");
+double ay = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты с точки А");
+double ac = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты y точки В");
+double bx = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты x точки В");
+double by = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты с точки В");
+double bc = int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(bx - ax, 2) + Math.Pow(by - ay, 2) + Math.Pow(bc - ac, 2));
+
+Console.WriteLine($"Расстоние между точками равно {Math.Round(result,2)}");
